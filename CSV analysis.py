9134cb04288f5ab3d73b5cf80b603f7a3918eb13
mikeymizike <45925345+mikeymizike@users.noreply.github.com>
2021-02-15 15:50:45 -0500

    Update CSV analysis.py

--- CSV analysis.py
+++ CSV analysis.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
 from tqdm import tqdm
+
 securities = pd.read_csv('optionable securities.csv')
 securities = securities['Symbol'].tolist()
-
-
-# In[111]:
-
 
 comments_df = pd.read_csv('reddit_data.csv').set_index('Time & Date').drop(columns=['Unnamed: 0']).sort_values(by='Time & Date', ascending=False)
 comments = comments_df['body'].tolist()
@@ -24,14 +15,4 @@ for x in tqdm(range(0, len(comments))):
                 if securities[i] == word:
                     tickers.append(word)
 
-
-# In[112]:
-
-
-pd.Series(tickers).value_counts().drop(['DD','A','USD', 'YOLO', 'ITM','CEO'])#[0:25]
-
-
-
-
-
-
+pd.Series(tickers).value_counts().drop(['DD','A','USD', 'YOLO', 'ITM','CEO'])[0:25]
